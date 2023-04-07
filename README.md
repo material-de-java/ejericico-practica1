765da30641c4fd4b2d15d0e36f57ed841670a3f9
Sergio Perez <dark07perez@gmail.com>
2023-04-06 22:17:32 -0500

    up

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ejericico-practica1
-ejercicio - sql - java - apirest
+# ejercicio - sql - java - apirest
+
+La Empresa XYZ cuenta con empleados de manera que:
+
+1. Cada empleado tiene la siguiente información: nombre, codigo, puesto de trabajo, sueldo, una referencia a su superior directo y una referencia al grupo al que pertenece.
+2. cada grupo tiene información sobre: nombre de oficina, codigo del área, una lista de referencia a sus empleados.
+3. El superior directo tambien en un empleado
+
+Relizar
+
+1. CRUD
+2. Uso de MVC, Hibernate, MySQL
+3. Crear servicio web que permita consultar los empleados de Empresa XYZ
+
+# Estructura
+
+Java, microservicios APIRest con SpringBoot sobre HTML y JS.
+
+1. Controllers
+   1. EmpleadoController (C)
+2. Models
+   1. Empleado (C)
+3. DAO
+   1. DAO (I)
+   2. EmpleadoDAOImpl (C:DAO)
+
--- SQL/sentencias.sql
+++ SQL/sentencias.sql
@@ -1,40 +1,36 @@
-CREATE SCHEMA `EMPRESAXYZ`;
-
-CREATE TABLE Grupo (
+CREATE TABLE EMPRESAXYZ.Grupo (
   id INT PRIMARY KEY,
   nombre_oficina VARCHAR(100) NOT NULL,
   codigo_area INT
 );
 
 
-CREATE TABLE Empleado (
+CREATE TABLE EMPRESAXYZ.Empleado (
   id INT PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   codigo INT,
   puesto VARCHAR(100) NOT NULL,
   sueldo DECIMAL(20,4) NOT NULL,
-  id_superior INT,
-  id_gr INT
+  id_superior INT
 );
 
 
-CREATE TABLE EmpleadoxGrupo (
+CREATE TABLE EMPRESAXYZ.EmpleadoxGrupo (
   id_em INT,
   id_gr INT,
   PRIMARY KEY (id_em, id_gr)
 )
 
-ALTER TABLE Empleado ADD CONSTRAINT FK_e_id_sup FOREIGN KEY (id_superior) REFERENCES Empleado(id);
-
-ALTER TABLE Empleado ADD CONSTRAINT FK_g_id_gr FOREIGN KEY (id_gr) REFERENCES Grupo(id);
+ALTER TABLE EMPRESAXYZ.Empleado ADD CONSTRAINT FK_e_id_sup FOREIGN KEY (id_superior) REFERENCES Empleado(id);
   
-ALTER TABLE EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_em FOREIGN KEY (id_em) REFERENCES Empleado(id);
+ALTER TABLE EMPRESAXYZ.EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_em FOREIGN KEY (id_em) REFERENCES Empleado(id);
 
-ALTER TABLE EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_gr FOREIGN KEY (id_gr) REFERENCES Grupo(id);
+ALTER TABLE EMPRESAXYZ.EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_gr FOREIGN KEY (id_gr) REFERENCES Grupo(id);
 
 
 
 DROP SCHEMA `EMPRESAXYZ`;
+CREATE SCHEMA `EMPRESAXYZ`;
 DROP TABLE EMPRESAXYZ.Empleado CASCADE;
 DROP TABLE EMPRESAXYZ.Grupo CASCADE;
 DROP TABLE EMPRESAXYZ.EmpleadoxGrupo CASCADE;
