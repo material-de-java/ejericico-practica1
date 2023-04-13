768e376254f523f655aef6dfe70b2c874960ecf0
Sergio Perez <dark07perez@gmail.com>
2023-04-12 20:26:11 -0500

    up

--- SQL/sentencias.sql
+++ SQL/sentencias.sql
@@ -1,31 +1,33 @@
+CREATE TABLE EMPRESAXYZ.Empleado (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(255) NOT NULL,
+  codigo INT,
+  puesto VARCHAR(100) NOT NULL,
+  sueldo DECIMAL(20,4) NOT NULL,
+  id_jefe INT,
+  id_grupo INT
+);
+
+
 CREATE TABLE EMPRESAXYZ.Grupo (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   nombre_oficina VARCHAR(100) NOT NULL,
   codigo_area INT
 );
 
 
-CREATE TABLE EMPRESAXYZ.Empleado (
-  id INT PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  codigo INT,
-  puesto VARCHAR(100) NOT NULL,
-  sueldo DECIMAL(20,4) NOT NULL,
-  id_superior INT
-);
-
-
 CREATE TABLE EMPRESAXYZ.EmpleadoxGrupo (
-  id_em INT,
-  id_gr INT,
-  PRIMARY KEY (id_em, id_gr)
+  # id_em INT,
+  # id_gr INT,
+  # PRIMARY KEY (id_em, id_gr)
 )
 
-ALTER TABLE EMPRESAXYZ.Empleado ADD CONSTRAINT FK_e_id_sup FOREIGN KEY (id_superior) REFERENCES Empleado(id);
+ALTER TABLE EMPRESAXYZ.Empleado ADD CONSTRAINT FK_e_id_jef FOREIGN KEY (id_jefe) REFERENCES Empleado(id);
+ALTER TABLE EMPRESAXYZ.Empleado ADD CONSTRAINT FK_e_id_gru FOREIGN KEY (id_grupo) REFERENCES Grupo(id);
   
-ALTER TABLE EMPRESAXYZ.EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_em FOREIGN KEY (id_em) REFERENCES Empleado(id);
+# ALTER TABLE EMPRESAXYZ.EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_em FOREIGN KEY (id_em) REFERENCES Empleado(id);
 
-ALTER TABLE EMPRESAXYZ.EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_gr FOREIGN KEY (id_gr) REFERENCES Grupo(id);
+# ALTER TABLE EMPRESAXYZ.EmpleadoxGrupo ADD CONSTRAINT FK_exg_id_gr FOREIGN KEY (id_gr) REFERENCES Grupo(id);
 
 
 
